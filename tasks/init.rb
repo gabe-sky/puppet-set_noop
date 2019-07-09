ce35df66a9b5e664a11dccf0a68882bd917cdcd0
Gabriel M Schuyler <gabe@puppetlabs.com>
2019-07-09 13:26:56 -0500

    Add logic to fully-qualify the correct path to the puppet binary
    Prior to this, the task used the unqualified `puppet` command.  This update
    examines the value of os.family to determine the correct path to the binary
    whether we're on Windows or not.

--- tasks/init.rb
+++ tasks/init.rb
@@ -4,16 +4,26 @@ require 'open3'
 require 'puppet'
 
 def set(truefalse)
-  cmd = ['puppet','config','set','--section','agent','noop',"#{truefalse}"]
+  # Step one, decide on the correct path to the Puppet binary.
+  if Facter.value(:os)['family'] == 'windows'
+    puppet_command = "C:/Program Files/Puppet Labs/Puppet/bin/puppet.bat"
+  else
+    puppet_command = '/opt/puppetlabs/puppet/bin/puppet'
+  end
+
+  # Now, use Puppet itself to update its config with the proper value of noop
+  cmd = [puppet_command,'config','set','--section','agent','noop',"#{truefalse}"]
   stdout, stderr, status = Open3.capture3(*cmd)
   raise Puppet::Error, _("stderr: ' #{stderr}') % { stderr: stderr }") if status != 0
   { status: stdout.strip }
   result = { 'result':"Set noop = #{truefalse} in puppet.conf" }
 end
 
+# Find the desired setting from the JSON coming in over STDIN
 params = JSON.parse(STDIN.read)
 truefalse = params['setting']
 
+# Run the command with the desired setting, and return the result
 begin
   result = set(truefalse)
   puts result.to_json
